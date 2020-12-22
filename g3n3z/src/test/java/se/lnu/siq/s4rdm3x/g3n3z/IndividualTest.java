ba60392a4a9d293fc111db2606dec6e35fedcc6c
Tobias Olsson <tobias.olsson@lnu.se>
2020-12-22 08:33:38 +0100

    more testing, these should be moved to the dev branch eventually.

--- g3n3z/src/test/java/se/lnu/siq/s4rdm3x/g3n3z/IndividualTest.java
+++ g3n3z/src/test/java/se/lnu/siq/s4rdm3x/g3n3z/IndividualTest.java
@@ -14,7 +14,9 @@ import se.lnu.siq.s4rdm3x.model.CGraph;
 import se.lnu.siq.s4rdm3x.model.cmd.mapper.ArchDef;
 import se.lnu.siq.s4rdm3x.model.cmd.mapper.MapperBase;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -79,22 +81,119 @@ class IndividualTest {
     }
 
     @Test
+    public void testTeammates_HugMe_1() {
+        TeammatesDump teammates = new TeammatesDump();
+        ArchDef a = teammates.m_a;
+        CGraph g = teammates.m_g;
+        Map<dmDependency.Type, ExperimentRunner.RandomDoubleVariable> dw = new HashMap<>();
+        for (dmDependency.Type t : dmDependency.Type.values()) {
+            dw.put(t, new ExperimentRunner.RandomDoubleVariable(1.0, 0));
+        }
+
+        HuGMeExperimentRun exp = new HuGMeExperimentRun(false, new ExperimentRunner.RandomDoubleVariable(0, 0), new ExperimentRunner.RandomDoubleVariable(1, 0), dw);
+        final ExperimentRunData.BasicRunData rd = exp.createNewRunData(new Random());
+        rd.m_totalMapped = a.getMappedNodeCount(g.getNodes());
+        a.getClusteredNodes(g.getNodes(), ArchDef.Component.ClusteringType.Initial).forEach(n -> rd.addInitialClusteredNode(n));
+        rd.m_initialClusteringPercent = (double) rd.getInitialClusteringNodeCount() / (double) rd.m_totalMapped;
+        while (!exp.runClustering(g, a));
+
+        assertEquals(0.303886925795053, rd.calcF1Score());
+
+        a.cleanNodeClusters(g.getNodes(), true);
+        for (dmDependency.Type t : dmDependency.Type.values()) {
+            dw.put(t, new ExperimentRunner.RandomDoubleVariable(0.0, 0));
+        }
+        dw.put(dmDependency.Type.Extends, new ExperimentRunner.RandomDoubleVariable(1.0, 0));
+        final ExperimentRunData.BasicRunData rd2= exp.createNewRunData(new Random());
+        rd2.m_totalMapped = a.getMappedNodeCount(g.getNodes());
+        a.getClusteredNodes(g.getNodes(), ArchDef.Component.ClusteringType.Initial).forEach(n -> rd2.addInitialClusteredNode(n));
+        rd2.m_initialClusteringPercent = (double) rd.getInitialClusteringNodeCount() / (double) rd2.m_totalMapped;
+        while (!exp.runClustering(g, a));
+
+        assertEquals(0.303886925795053, rd2.calcF1Score());
+
+
+    }
+
+    @Test
+    public void testTeammates_HugMe_2() {
+        TeammatesDump teammates = new TeammatesDump();
+        ArchDef a = teammates.m_a;
+        CGraph g = teammates.m_g;
+        Map<dmDependency.Type, ExperimentRunner.RandomDoubleVariable> dw = new HashMap<>();
+        for (dmDependency.Type t : dmDependency.Type.values()) {
+            dw.put(t, new ExperimentRunner.RandomDoubleVariable(0.0, 0));
+        }
+        dw.put(dmDependency.Type.Extends, new ExperimentRunner.RandomDoubleVariable(1.0, 0));
+        dw.put(dmDependency.Type.Implements, new ExperimentRunner.RandomDoubleVariable(0.07479101885668538, 0));
+        //dw.put(dmDependency.Type.Returns, new ExperimentRunner.RandomDoubleVariable( 0.22679069305507837,  0));
+        //dw.put(dmDependency.Type.ConstructorCall, new ExperimentRunner.RandomDoubleVariable( 0.16623343264692425,  0));
+        //dw.put(dmDependency.Type.OwnFieldUse, new ExperimentRunner.RandomDoubleVariable( 0.06420987518192389,  0));
+        dw.put(dmDependency.Type.Field, new ExperimentRunner.RandomDoubleVariable( 0.10568346526641301,  0));
+
+        HuGMeExperimentRun exp = new HuGMeExperimentRun(false, new ExperimentRunner.RandomDoubleVariable(0, 0), new ExperimentRunner.RandomDoubleVariable(1, 0), dw);
+        final ExperimentRunData.BasicRunData rd = exp.createNewRunData(new Random());
+        rd.m_totalMapped = a.getMappedNodeCount(g.getNodes());
+        a.getClusteredNodes(g.getNodes(), ArchDef.Component.ClusteringType.Initial).forEach(n -> rd.addInitialClusteredNode(n));
+        rd.m_initialClusteringPercent = (double) rd.getInitialClusteringNodeCount() / (double) rd.m_totalMapped;
+        while (!exp.runClustering(g, a));
+
+        assertEquals(0.8476454293628809, rd.calcF1Score());
+    }
+
+    @Test
+    public void testTeammates_HugMe_3() {
+        TeammatesDump teammates = new TeammatesDump();
+        ArchDef a = teammates.m_a;
+        CGraph g = teammates.m_g;
+        Map<dmDependency.Type, ExperimentRunner.RandomDoubleVariable> dw = new HashMap<>();
+        for (dmDependency.Type t : dmDependency.Type.values()) {
+            dw.put(t, new ExperimentRunner.RandomDoubleVariable(0.0, 0));
+        }
+        dw.put(dmDependency.Type.Extends, new ExperimentRunner.RandomDoubleVariable(1.0, 0));
+        //dw.put(dmDependency.Type.Implements, new ExperimentRunner.RandomDoubleVariable(0.07479101885668538, 0));
+        //dw.put(dmDependency.Type.Returns, new ExperimentRunner.RandomDoubleVariable( 0.22679069305507837,  0));
+        //dw.put(dmDependency.Type.ConstructorCall, new ExperimentRunner.RandomDoubleVariable( 0.16623343264692425,  0));
+        //dw.put(dmDependency.Type.OwnFieldUse, new ExperimentRunner.RandomDoubleVariable( 0.06420987518192389,  0));
+        //dw.put(dmDependency.Type.Field, new ExperimentRunner.RandomDoubleVariable( 0.10568346526641301,  0));
+
+        HuGMeExperimentRun exp = new HuGMeExperimentRun(false, new ExperimentRunner.RandomDoubleVariable(0, 0), new ExperimentRunner.RandomDoubleVariable(1, 0), dw);
+        final ExperimentRunData.BasicRunData rd = exp.createNewRunData(new Random());
+        rd.m_totalMapped = a.getMappedNodeCount(g.getNodes());
+        a.getClusteredNodes(g.getNodes(), ArchDef.Component.ClusteringType.Initial).forEach(n -> rd.addInitialClusteredNode(n));
+        rd.m_initialClusteringPercent = (double) rd.getInitialClusteringNodeCount() / (double) rd.m_totalMapped;
+        while (!exp.runClustering(g, a));
+
+        assertEquals(0.8228228228228228, rd.calcF1Score());
+    }
+
+
+    @Test
     public void dumpSystem() {
-        try {
+        /*try {
             System2JavaDumper s2jd = new System2JavaDumper();
 
-            FileBased system = new FileBased("C:/hObbE/projects/coding/github/s4rdm3x/data/systems/ProM6.9/ProM_6_9.sysmdl");
-            //FileBased system = new FileBased("C:/hObbE/projects/coding/github/s4rdm3x/data/systems/teammates/teammates.sysmdl");
+            //FileBased system = new FileBased("C:/hObbE/projects/coding/github/s4rdm3x/data/systems/ProM6.9/ProM_6_9.sysmdl");
+            FileBased system = new FileBased("C:/hObbE/projects/coding/github/s4rdm3x/data/systems/teammates/teammates.sysmdl");
             CGraph g = new CGraph();
             system.load(g);
             ArchDef a = system.createAndMapArch(g);
 
+            Random r = new Random(171717);
+            Rand rm = new Rand();
+            rm.assignMetric(a.getMappedNodes(g.getNodes()));
 
-            s2jd.dump(java.lang.System.out, "Testing", g);
+            InitialSetGenerator isg = new InitialSetGenerator();
+            isg.assignInitialClusters(g, a, 0.75, new Rand(), r);
+
+            File out = new File("C:/hobbe/projects/coding/github/s4rdm3x/g3n3z/src/test/java/se/lnu/siq/s4rdm3x/g3n3z/TeammatesDump.java");
+            s2jd.dump(new PrintStream(out), "Testing", g, a);
+
+
         } catch (Exception e) {
             e.printStackTrace();
             assertTrue(false);
-        }
+        }*/
     }
 
 }
