ff2cc24b3ba5d0273a2ed7376b1595c651287417
Tobias Olsson <tobias.olsson@lnu.se>
2020-12-22 20:03:28 +0100

    improved the dumping with more testing and a base class.

---
+++ src/test/java/se/lnu/siq/s4rdm3x/experiments/regression/dumps/DumpBase.java
@@ -0,0 +1,23 @@
+package se.lnu.siq.s4rdm3x.experiments.regression.dumps;
+
+import se.lnu.siq.s4rdm3x.dmodel.dmClass;
+import se.lnu.siq.s4rdm3x.dmodel.dmDependency;
+import se.lnu.siq.s4rdm3x.model.CGraph;
+import se.lnu.siq.s4rdm3x.model.cmd.mapper.ArchDef;
+
+import java.util.HashMap;
+
+public class DumpBase {
+    public CGraph m_g = new CGraph();
+    public ArchDef m_a = new ArchDef();
+    public HashMap<String, dmClass> m_classes = new HashMap<>();
+
+    public void d(dmClass a_source, dmClass a_target, dmDependency.Type a_dt, int[] a_lines) {
+        for (int i : a_lines) {
+            a_source.addDependency(a_target, a_dt, i);
+        }
+    }
+    public void d(dmClass a_source, dmClass a_target, dmDependency.Type a_dt, int a_line) {
+        a_source.addDependency(a_target, a_dt, a_line);
+    }
+}
